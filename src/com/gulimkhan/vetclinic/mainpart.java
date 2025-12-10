1708b2ebb649b734e67e3010787b4a072879a908
gulimkhaan <254963@astanait.edu.kz>
2025-12-10 10:31:12 +0500

    initial comment: Vet Clinic Project

---
+++ src/com/gulimkhan/vetclinic/mainpart.java
@@ -0,0 +1,13 @@
+package com.gulimkhaan.vetclinic;
+
+public class mainpart {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Vet Clinic Management System!");
+        System.out.println("This system will help manage pets and appointments.");
+        System.out.println("Main features planned:");
+        System.out.println(" - Register pets and owners");
+        System.out.println(" - Schedule and manage appointments");
+        System.out.println(" - Keep medical records and vaccination history");
+        System.out.println(  "Developed by: Гүлімхан | Course: OOP");
+    }
+}
